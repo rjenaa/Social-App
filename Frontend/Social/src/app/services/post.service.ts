c7035b7e2f126c795d3cc84b3aa6f7838bf32b97
Darren152 <tdarren27@gmail.com>
2020-11-28 19:25:14 -0500

    completed services for post and resource

--- Frontend/Social/src/app/services/post.service.ts
+++ Frontend/Social/src/app/services/post.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Post} from "../models/Post";
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostService {
 
-  constructor() { }
+  private URL = 'http://localhost:8080';
+
+  constructor(private http: HttpClient) { }
+
+  fetchAllPost(): Observable<any>{
+    return this.http.get<any>(this.URL+`/post/`);
+  }
+
+  fetchPostById(id: number): Observable<any>{
+    return this.http.get<any>(this.URL+`/post/${id}`);
+  }
+
+  fetchPostByAccountId(accountId: number): Observable<any>{
+    return this.http.get<any>(this.URL+`/post/${accountId}`);
+  }
+
+  createAPost(post: Post): Observable<any>{
+    return this.http.post<any>(this.URL+`/post/`, post)
+  }
+
+  deleteAPost(id: number): Observable<any>{
+    return this.http.delete<any>(this.URL+`/post/${id}`);
+  }
 }
